9746780176ab3f59faf09b6fa054e0e25b6f367a
mattjared <mattjared9@gmail.com>
2018-10-14 16:58:50 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/binge-board/precache-manifest.9d80f86d93dfb5b7cdf7277cb0bdf1d0.js"
+  "/precache-manifest.3b051fc85aa6f6bd8a8288d23a780687.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/binge-board/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
