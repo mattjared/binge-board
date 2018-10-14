a68e62bc087d269441ba7cdedb8527ce91d7c4f6
mattjared <mattjared9@gmail.com>
2018-10-14 16:15:40 -0500

    Updates

---
+++ precache-manifest.7a0c52b61d100355df34e8d0f055db82.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef67dd215b8353fd97c0",
+    "url": "/binge-board/static/js/runtime~main.ef67dd21.js"
+  },
+  {
+    "revision": "43d0f762685ea983e404",
+    "url": "/binge-board/static/js/main.43d0f762.chunk.js"
+  },
+  {
+    "revision": "ad4591f84e9b761fa829",
+    "url": "/binge-board/static/js/1.ad4591f8.chunk.js"
+  },
+  {
+    "revision": "43d0f762685ea983e404",
+    "url": "/binge-board/static/css/main.5c7d3875.chunk.css"
+  },
+  {
+    "revision": "364004799f575023055877bdf772a4e9",
+    "url": "/binge-board/index.html"
+  }
+];
