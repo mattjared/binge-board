d800ebeb05daf96b59945d4f76e9c30c7d92f752
mattjared <mattjared9@gmail.com>
2018-10-14 17:18:56 -0500

    Add analytics

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
-import { data } from './data.js'
+import { data } from './data.js';
 import Button from './Button';
+import ReactGA from 'react-ga';
 
 class App extends Component {
+  initializeReactGA() {
+    ReactGA.initialize('UA-127457647-1');
+    ReactGA.pageview('/homepage');
+  }
   render() {
     return (
-      <div className="App">
+      <div className="App" onLoad={this.initializeReactGA}>
         <h2 className="App-header">Binge Board</h2>
         <div className="App-wrapper">
           {data.map((d, i) => {
@@ -16,7 +21,7 @@ class App extends Component {
                 name={d.name}
                 audio={d.audio}
               />
-            )
+            ) 
           })}
         </div>
       </div>
