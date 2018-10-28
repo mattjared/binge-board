0070f52521df02b3b84bf7e53c36d50a806525a9
mattjared <mattjared9@gmail.com>
2018-10-28 15:35:44 -0500

    lots more audio

--- src/data.js
+++ src/data.js
@@ -8,7 +8,27 @@ export const data = [
     "audio": './sounds/cardinal.mp3',
   },
   {
+    "name": "💸  Bag Man",
+    "audio": './sounds/money.mp3',
+  },
+  {
     "name": "Seamus 🔥",
     "audio": './sounds/seamus.mp3',
-  }
+  },
+  {
+    "name": "McGalleon Needs Her Fix 💰",
+    "audio": './sounds/mcgalleon.mp3',
+  },
+  {
+    "name": "🛑 Warning!",
+    "audio": './sounds/warning.mp3',
+  },
+  {
+    "name": "What a great website",
+    "audio": './sounds/whatagreatwebsite.mp3',
+  },
+  {
+    "name": "Fluer 👶",
+    "audio": ['./sounds/fluer1.mp3', './sounds/fluer2.mp3', './sounds/fluer3.mp3'],
+  },
 ] 
