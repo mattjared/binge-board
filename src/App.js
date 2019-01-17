c2e51e948d98c6228b1a2010ff1e9cf4da43973a
mattjared <mattjared9@gmail.com>
2019-01-16 19:43:46 -0600

    [got] animation attempt #2

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import potter from './potter.js';
 import got from './got.js';
 import Button from './Button';
 import ReactGA from 'react-ga';
+import CSSTransition from 'react-transition-group/Transition';
 
 ReactGA.initialize('UA-127457647-1');
 ReactGA.pageview('/homepage');
@@ -23,31 +24,39 @@ class App extends Component {
   render() {
     const date = new Date();
     const year = date.getFullYear();
-    let data;
     let emoji;
+    let data;
     if (this.state.boardType) {
-      data = potter;
       emoji = "⚡";
+      data = potter;
     } else {
-      data = got; 
       emoji = "👑";
+      data = got;
     }
-
     return (
       <div className="App">
         <h2 onClick={this.changeBoard}>BINGE <span role="img" aria-label="Lightning Bolt">{emoji}</span>️ BOARD</h2>
         <p><span role="img" aria-label="Speaker">🔈</span> Sound on <span role="img" aria-label="Speaker">🔈</span></p>
         <div className="App-wrapper">
-          {data.map((d, i) => {
-            return (
-              <Button
-                onButtonClick={this.playAudio}
-                key={d.name+i}  
-                name={d.name}
-                audio={d.audio}
-              />
-            ) 
-          })}
+          <CSSTransition
+            timeout={3000}
+            classNames="card"
+            unmountOnExit
+            in={this.state.boardType}
+          >
+          <div>
+            {data.map((d, i) => {
+              return (
+                <Button
+                  onButtonClick={this.playAudio}
+                  name={d.name}
+                  audio={d.audio}
+                  key={d.name+i}
+                />
+              ) 
+            })}
+            </div> 
+          </CSSTransition>
         </div>
         <div className="App-footer">
           <p>Source: The Ringer's <a href="https://www.theringer.com/binge-mode" target="blank">Binge Mode</a> podcast. Copyright &copy; { year }. Enjoy using the Binge Board? <a href="https://buymeacoff.ee/mattjared" target="_blank" rel="noopener noreferrer">Buy me a coffee!</a></p>
